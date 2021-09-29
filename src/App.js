c5bf43b63e66e9591f02530ee615874090c3c11e
Sobhie <sobhie.saad1@hotmail.com>
2021-09-29 00:48:41 -0700

    netflix clone with firebase hosting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import Row from './Row';
+import requests from './requests.js';
+import Banner from './Banner'
+import Nav from './Nav'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/**Navbar */}
+      <Nav />
+
+      {/** Banner */}
+      <Banner />
+
+      {/** movies rows */}
+        <Row  title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} 
+        isLargeRow
+        />
+        <Row  title="Trending Now" fetchUrl= {requests.fetchTrending}/>
+        <Row  title="Top Rated" fetchUrl= {requests.fetchTopReated}/>
+        <Row  title="Action Movies" fetchUrl= {requests.fetchActionMovies}/>
+        <Row  title="Comedy Movies" fetchUrl= {requests.fetchComedyMovies}/>
+        <Row  title="Horro Movies" fetchUrl= {requests.fetchHorrorMovies}/>
+        <Row  title="Romance Movies" fetchUrl= {requests.fetchRomanceMovies}/>
+        <Row  title="Documentaries" fetchUrl= {requests.fetchDocumentaries}/>
+
     </div>
   );
 }
